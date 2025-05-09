6f60086c7dd529f5036376d7a6dea1ea01042f72
Jonathan Frey <jonathanpaulfrey@gmail.com>
2025-05-09 11:13:49 +0200

    cleanup problem.py, removing unused classes NonlinearDiscreteDynamics, ControlBoundedOcp

--- benchmark_mpc_pytorch/problems.py
+++ benchmark_mpc_pytorch/problems.py
@@ -1,8 +1,6 @@
 from dataclasses import dataclass
 
-import torch
 import numpy as np
-import casadi as ca
 
 @dataclass
 class LinearDiscreteDynamics:
@@ -10,14 +8,6 @@ class LinearDiscreteDynamics:
     A: np.ndarray
     B: np.ndarray
     b: np.ndarray
-
-@dataclass
-class NonlinearDiscreteDynamics:
-    """Nonlinear discrete-time dynamics x_{t+1} = f(x_t, u_t)."""
-    f_pytorch: torch.nn.Module
-    f_casadi_expr: ca.SX
-    x: ca.SX
-    u: ca.SX
 
 @dataclass
 class QuadraticCost:
@@ -37,18 +27,6 @@ class ControlBounds:
 class ControlBoundedLqrProblem:
     """Linear quadratic regulator problem with control bounds."""
     dynamics: LinearDiscreteDynamics
-    cost: QuadraticCost
-    control_bounds: ControlBounds
-    N_horizon: int
-
-    def __post_init__(self):
-        self.nx, self.nu = self.dynamics.A.shape[0], self.dynamics.B.shape[1]
-
-
-@dataclass
-class ControlBoundedOcp:
-    """Linear quadratic regulator problem with control bounds."""
-    dynamics: NonlinearDiscreteDynamics
     cost: QuadraticCost
     control_bounds: ControlBounds
     N_horizon: int
